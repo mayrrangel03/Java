ec61a8dacc911486ccc4fdc4301083d276f791ba
mayara <mayara@itexto.com.br>
2022-05-03 15:17:17 -0300

    Meu primeiro commit

---
+++ SpringBoot/mayara_first/src/main/java/br/com/mayara/mayara_first/MayaraFirstApplication.java
@@ -0,0 +1,13 @@
+package br.com.mayara.mayara_first;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MayaraFirstApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MayaraFirstApplication.class, args);
+	}
+
+}
---
+++ SpringBoot/mayara_first/src/test/java/br/com/mayara/mayara_first/MayaraFirstApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.mayara.mayara_first;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MayaraFirstApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
